e6fb8098aec6b35b6bd0ed175b82197d7f31fe41
guanghui <995980020@qq.com>
2024-09-04 00:23:10 +0800

    feat(): 完善指令

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
@@ -11,7 +10,6 @@ export default defineConfig(() => {
     base: './',
     plugins: [
       vue(),
-      vueJsx(),
       Pages({
         dirs: './src/views', //指定生成路由的目录
         extensions: ['vue'], //文件后缀
